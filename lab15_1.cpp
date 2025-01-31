2f203e5048ed751f6dc8f131fff8c74ee1b25bb7
thanpisit-film-hub <tanpisit.2548@gmail.com>
2025-01-31 15:20:39 +0700

    Lab15

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,28 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
 
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int **z = &x;
+
+	cout << a << " " << b << " " << c << " " << x << " " << static_cast<void *>(&y) << " " << z << endl;
+	cout << &a << " " << static_cast<void *>(&b) << " " << static_cast<void *>(&c) << " " << &x << " " << &y << " " << &z << endl;
 	
-	
+	c = 'F';
+	cout << a << " " << b << " " << c << " " << x << " " << static_cast<void *>(&y) << " " << z << endl;
+
+	*y = 'W';
+	cout << a << " " << b << " " << c << " " << x << " " << static_cast<void *>(&y) << " " << z << endl;
+
+	*x = 6;
+	cout << a << " " << b << " " << c << " " << x << " " << static_cast<void *>(&y) << " " << z << endl;
+
+	**z = 7;
+	cout << a << " " << b << " " << c << " " << x << " " << static_cast<void *>(&y) << " " << z << endl;
 	return 0;
 }
--- lab15_5.cpp
+++ lab15_5.cpp
@@ -18,5 +18,26 @@ int main(){
 	return 0;
 }
 
-//Do not modify the code above this line
-//Write definition of shuffle() using pointer after this line. 
+void shuffle(int *a,int *b,int *c,int *d){
+	int arr[] = {50, 100, 500, 1000};
+	int num1, num2, num3, num4;
+	num1 = rand() % 4;
+	num2 = rand() % 4;
+	num3 = rand() % 4;
+	num4 = rand() % 4;
+
+	for(int i = 0; i < 10; i++){
+		if(num1 == num2) num2 = rand() % 4;
+		if(num1 == num3) num3 = rand() % 4;
+		if(num1 == num4) num4 = rand() % 4;
+
+		if(num2 == num3 || num2 == num1) num3 = rand() % 4;
+		if(num2 == num4 || num2 == num1) num4 = rand() % 4;
+
+		if(num3 == num4 || num3 == num1 || num3 == num2) num4 = rand() % 4;
+	}
+	*a = arr[num1];
+	*b = arr[num2];
+	*c = arr[num3];
+	*d = arr[num4];
+}
